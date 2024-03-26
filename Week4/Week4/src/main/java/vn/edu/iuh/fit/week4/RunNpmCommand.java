5deafeb8198cd250bf5ce376b2034ece03997e48
TranThienDatLaGi <134665779+TranThienDatLaGi@users.noreply.github.com>
2024-03-26 22:38:48 +0700

    change something

--- Week4/Week4/src/main/java/vn/edu/iuh/fit/week4/RunNpmCommand.java
+++ Week4/Week4/src/main/java/vn/edu/iuh/fit/week4/RunNpmCommand.java
@@ -5,23 +5,17 @@ import java.io.IOException;
 
 public class RunNpmCommand {
     public static void main(String[] args) {
-        // Đường dẫn đến thư mục gốc của dự án
         String projectDir = "D:/Hoc_tap/Ktruc&TkePhanMem/ThucHanh/Week4/Week4/jdepend-ui";
 
-        // Đường dẫn đến tệp XML báo cáo
         String xmlReportPath = "../reports/report.xml";
-        // Tiền tố của gói
         String packagePrefix = "vn.edu.iuh.week4";
 
-        // Xây dựng lệnh npm run jdepend-ui
         String npmCommand = "npm run jdepend-ui " + xmlReportPath + " " + packagePrefix;
 
         try {
-            // Di chuyển đến thư mục gốc của dự án
             ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", npmCommand);
             processBuilder.directory(new File(projectDir));
 
-            // Thực thi lệnh npm bằng cách sử dụng ProcessBuilder
             Process process = processBuilder.inheritIO().start();
             int exitCode = process.waitFor();
             if (exitCode == 0) {
